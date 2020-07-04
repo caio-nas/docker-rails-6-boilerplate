e016e696d5a731b50a3b7e2a0a152196ef884260
Caio Nascimento <contato@caionascimento.com.br>
2020-07-04 20:33:49 -0300

    First fully functional version

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 ```
 $ git clone --depth 1 git@github.com:caio-nas/docker-rails-6-boilerplate.git <YOUR_APP_NAME>
+
 $ cd <YOUR_APP_NAME>
-$ git remote remove origin
 $ docker-compose run --rm web bundle install
-$ docker-compose run --rm web bundle exec rails new . -m template.rb -d postgresql --skip-system-test --skip-action-text --skip-action-cable --skip-git -T # only for new projects
-$ docker-compose run --rm web yarn install
-$ docker-compose run --rm web bundle exec rails webpacker:install
+
+# only for new projects
+$ docker-compose run --rm web bundle exec rails new . -m template.rb -d postgresql --skip-system-test --skip-action-text --skip-action-cable --skip-git -f -T
+
+$ sudo chown -R $USER:$USER .
 $ docker-compose up -d
-$ docker-compose exec web ./bin/rails db:create
 ```
 
 You can see Rails top page on http://localhost:3000/.
--- template.rb
+++ template.rb
@@ -10,18 +10,23 @@ end
 
 def add_gems
   gem 'devise', '~> 4.7', '>= 4.7.1'
+  gem 'devise-i18n'
   gem 'sidekiq', '~> 6.1', '>= 6.1.0'
   gem 'therubyracer'
+  gem 'haml-rails', '~> 2.0'
   gem 'less-rails'
   gem 'twitter-bootstrap-rails'
 
-  gem 'pry-rails', :group => [:development, :test]
-  gem 'pry-byebug', :group => [:development, :test]
+  gem_group :development, :test do
+    gem 'rspec-rails'
+    gem 'pry-rails'
+    gem 'pry-byebug'
+  end
 end
 
 def add_users
   # Install Devise
-  generate "devise:install"
+  generate 'devise:install'
 
   # Configure Devise
   environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }",
@@ -30,30 +35,30 @@ def add_users
   route "root to: 'home#index'"
 
   # Create Devise User
-  generate :devise, "User", "username", "name", "admin:boolean"
+  generate :devise, 'User', 'username', 'name', 'admin:boolean'
 
   # set admin boolean to false by default
   in_root do
-    migration = Dir.glob("db/migrate/*").max_by{ |f| File.mtime(f) }
-    gsub_file migration, /:admin/, ":admin, default: false"
+    migration = Dir.glob('db/migrate/*').max_by { |f| File.mtime(f) }
+    gsub_file migration, /:admin/, ':admin, default: false'
   end
 end
 
 def copy_templates
-  directory "app", force: true
+  directory 'app_template', 'app', force: true
+  directory 'config_template', 'config', force: true
 end
 
 def add_bootstrap
   run 'bundle exec rails g bootstrap:install less'
-  run 'bundle exec rails g bootstrap:layout application'
 end
 
 def add_sidekiq
-  environment "config.active_job.queue_adapter = :sidekiq"
+  environment 'config.active_job.queue_adapter = :sidekiq'
 
-  insert_into_file "config/routes.rb",
+  insert_into_file 'config/routes.rb',
     "require 'sidekiq/web'\n\n",
-    before: "Rails.application.routes.draw do"
+    before: 'Rails.application.routes.draw do'
 
   content = <<-RUBY
     authenticate :user, lambda { |u| u.admin? } do
@@ -64,37 +69,50 @@ def add_sidekiq
   insert_into_file "config/routes.rb", "#{content}\n\n", after: "Rails.application.routes.draw do\n"
 end
 
-def remove_template_file
-  remove_file "template.rb"
+def cleanup
+  remove_file 'template.rb'
+  remove_file 'app/views/layouts/application.html.erb'
+  remove_dir 'app_template'
+  remove_dir 'config_template'
+  run 'git remote remove origin'
+end
+
+def configure_locale
+  environment "config.i18n.default_locale = 'pt-BR'"
+end
+
+def setup_git
+  run "git config --global user.email 'caio.mvnascimento@gmail.com'"
+  run "git config --global user.name 'Caio Nascimento'"
+
+  git :init
+  git add: '.'
+  git commit: %Q{ -m 'Project Initial commit' }
 end
 
 # Main setup
 source_paths
-
+configure_locale
 add_gems
 
 after_bundle do
-  add_users
-  add_sidekiq
   copy_templates
   add_bootstrap
+  add_sidekiq
 
   # Migrate
-  rails_command "db:create"
-  rails_command "db:migrate"
+  add_users
+  rails_command 'db:migrate:reset'
 
-  remove_template_file
-
-#   git :init
-#   git add: "."
-#   git commit: %Q{ -m "Initial commit" }
+  cleanup
+  setup_git
 
   say
-  say "Kickoff app successfully created! 👍", :green
+  say 'Kickoff app successfully created! 👍', :green
   say
-  say "Switch to your app by running:"
-  say "$ cd #{app_name}", :yellow
+  say 'Switch to your app by running:'
+  say '$ cd #{app_name}', :yellow
   say
-  say "Then run:"
-  say "$ rails server", :green
+  say 'Then run:'
+  say '$ rails server', :green
 end
